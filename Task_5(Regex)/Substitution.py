136c5611d41d7036b4971c0f133963d6b1bf7e2a
Yug Raval <180540107140@darshan.ac.in>
2021-10-22 22:50:42 +0530

    Regex Task

---
+++ Task_5(Regex)/Substitution.py
@@ -0,0 +1,7 @@
+for _ in range(int(input())):
+    line = input()
+    
+    while ' && ' in line or ' || ' in line:
+        line = line.replace(" && ", " and ").replace(" || ", " or ")
+    
+    print(line)
